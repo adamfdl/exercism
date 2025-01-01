6aac3d3af58cd9855307011dc504729a131ae7ea
Adam Fadhil <xen.n-adam@ip-192-168-1-3.ap-southeast-1.compute.internal>
2025-01-01 21:49:27 +0700

    grains

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,33 @@
 #include "grains.h"
+
+uint64_t power_uint64(uint64_t base, uint64_t exponent) {
+  uint64_t result = 1;
+  for (uint64_t i=0; i<exponent; i++) {
+    // Repeat multiplying result by base for as many
+    // exponent times
+    result = result * base;
+  }
+  return result;
+}
+
+uint64_t square(uint8_t index) {
+  if (index == 0 || index > 64) {
+    return 0;
+  }
+
+  /**
+   * Formula is:
+   *    a_n = 2^(n-1)
+   * Pattern:
+   *    1,2,4,8,16,32
+   */
+  return power_uint64(2, index-1);
+}
+
+uint64_t total(void) {
+  uint64_t result = 0;
+  for (int i=1; i<=64; i++) {
+    result = result + square(i);
+  }
+  return result;
+}
--- c/grains/grains.h
+++ c/grains/grains.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+uint64_t power_uint64(uint64_t base, uint64_t exponent);
 uint64_t square(uint8_t index);
 uint64_t total(void);
 
